2c353282355c8af64e667c0baf5b35d652ccf189
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2017-03-12 11:21:55 +0100

    Add Leiningen boilerplate code

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to it.couchgames.kata.string-calculator
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/it/couchgames/kata/string_calculator_test.clj
@@ -0,0 +1,7 @@
+(ns it.couchgames.kata.string-calculator-test
+  (:require [clojure.test :refer :all]
+            [it.couchgames.kata.string-calculator :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
