56a62c105c3103c466a8d4533a092577acf85cae
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2017-03-12 11:25:46 +0100

    Test "empty string" passes

--- src/it/couchgames/kata/string_calculator.clj
+++ src/it/couchgames/kata/string_calculator.clj
@@ -1,6 +1,9 @@
 (ns it.couchgames.kata.string-calculator
   (:gen-class))
 
+
+(defn string-calculator [s] 0)
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
--- test/it/couchgames/kata/string_calculator_test.clj
+++ test/it/couchgames/kata/string_calculator_test.clj
@@ -2,6 +2,6 @@
   (:require [clojure.test :refer :all]
             [it.couchgames.kata.string-calculator :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest basic-single-numbers-tests
+  (testing "emtpy string"
+    (is (= 0 (string-calculator "")))))
